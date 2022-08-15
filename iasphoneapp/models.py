ad9cfa8a835c677b3ebc72a9c2852887f841c9a8
Ivan Pavic <ivan.pavic@Ivans-MacBook-Pro-14.local>
2022-08-15 22:16:45 +0200

    Removed field from person class

--- iasphoneapp/models.py
+++ iasphoneapp/models.py
@@ -6,4 +6,3 @@ class Person(models.Model):
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     email = models.EmailField(max_length=254)
-    created_at = models.DateTimeField(auto_now_add=True)
