aaa0797863a66a76b899129de2a8681951bcf32a
Ivan Pavic <ivan.pavic@Ivans-MacBook-Pro-14.local>
2022-08-15 21:12:53 +0200

    fix

--- iasphone/settings.py
+++ iasphone/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7wpa)!=5&_mk5+sq*++plfid$9bltfp&g&fqvcr+x$zt#izjd2'
+# SECRET_KEY = 'django-insecure-7wpa)!=5&_mk5+sq*++plfid$9bltfp&g&fqvcr+x$zt#izjd2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
