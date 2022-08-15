b1b52b2a0b4d97b871c15bded39b0c5ce2d4a229
Ivan Pavic <ivan.pavic@Ivans-MacBook-Pro-14.local>
2022-08-15 22:37:34 +0200

    added def to person model

--- iasphoneapp/models.py
+++ iasphoneapp/models.py
@@ -6,3 +6,6 @@ class Person(models.Model):
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.email
