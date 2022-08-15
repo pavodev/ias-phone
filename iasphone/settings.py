ae2e2d4b2477250f25f90ecfdd4a57d4c3319702
Ivan Pavic <ivan96jp@gmail.com>
2022-08-15 22:50:48 +0200

    fixed typo in settings.py

--- iasphone/settings.py
+++ iasphone/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'iasphoneapp'
-    'rest_framework',
+    'iasphoneapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
