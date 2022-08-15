fa89805c7879ce9cc27b79c98a3c6ae7503ce0e3
Ivan Pavic <ivan.pavic@Ivans-MacBook-Pro-14.local>
2022-08-15 22:43:48 +0200

    Implemented Person api

--- iasphoneapp/views.py
+++ iasphoneapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from iasphoneapp.models import Person
+from iasphoneapp.serializers import PersonSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
